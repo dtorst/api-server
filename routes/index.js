aa76e037088ed16548db01b4bde57f27ce57d68d
David Torstenson <david.torstenson@hulu.com>
2014-10-20 18:05:09 -0700

    making notes for next steps in index.js

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,16 @@ var twilio = require('./twilio');
 // AUTH routes
 router.post('/api/v1/auth/register', auth.register);
 router.post('/api/v1/auth/login', auth.login);
+// NEW ONE HERE: eventually will want to add an edit section for making changes to accounts
 
 // REMINDER routes
-router.get('/api/v1/reminders/:userId', reminder.getAll);
+router.get('/api/v1/reminders/:userId', reminder.getAll); // eventually this will serve as the history API, to show karma points and how a user got there
 router.get('/api/v1/reminder/:userId/:reminderId', reminder.getOne);
-router.post('/api/v1/reminder/:userId/create', reminder.create);
-router.put('/api/v1/reminder/:userId/:reminderId/cancel', reminder.cancel);
-router.delete('/api/v1/reminder/:userId/:reminderId', reminder.delete);
+router.post('/api/v1/reminder/:userId/create', reminder.create); // this will be the main one to work on at first
+router.put('/api/v1/reminder/:userId/:reminderId/cancel', reminder.cancel); // will eventually deprecate
+router.delete('/api/v1/reminder/:userId/:reminderId', reminder.delete); // might deprecate as well
 
 // TWILIO Call route - TWIML Response
-router.post('/call/twiml/:userId/:reminderId', twilio.getTwiml);
+router.post('/call/twiml/:userId/:reminderId', twilio.getTwiml); // eventually will need to figure out how to fire this without a scheduled time, i.e. as soon as it's sent
 
 module.exports = router;
